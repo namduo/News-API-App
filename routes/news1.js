79d13a487536ac067f0dc532f987f54de22f0d75
FemkeBuijs <32512274+FemkeBuijs@users.noreply.github.com>
2017-10-20 11:53:07 +0100

    Create news1 file & home
    Create news1 file & home

--- routes/news1.js
+++ routes/news1.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-//
 var path = require('path');
 
-/* GET about page. */
+
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.sendFile(path.join(__dirname, '../views/news1.html'));
 });
 
