408f9302dadf79420b42a845f317164082fddc36
Nam Nguyen <nm.duong@outlook.com>
2017-10-26 20:49:32 +0100

    News1 Update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,3 @@
-$.ajax('/news1/api', {
-    method: "GET",
-
-    success: function(data){
-    var myData = JSON.parse(data);
-    console.log(myData);
-
-    // for (var i = 0; i < myData.articles.length; i++) {
-    //   // console.log(myData.articles[i]);
-    //   // $('.newsAuthor').append(myData.articles[i].author);
-    // }
-
-    },
-    error:function(error){
-        console.log('error: ' + error)
-    }
-});
-
 $.ajax('/api', {
   method: "GET",
 
@@ -23,34 +5,17 @@ $.ajax('/api', {
   var myData = JSON.parse(data);
   console.log(myData);
 
-  // $('.url').attr("href", myData.sources[4].url);
-  // $('.name').append(myData.sources[4].name);
-  // $('.description').append(myData.sources[4].description);
-
-
+  var textTitle = '<ul class="">';
 
   for (var i = 0; i < myData.sources.length; i++) {
 
-    var sourceTitle = $("<a>").attr("href", myData.sources[i].url).append($("<h2>", { text: myData.sources[i].name }).addClass("name"));
-    $(".sourceContainer").append(sourceTitle);
-
-    // for (var ii = 0; ii < myData.sources[i].description.length; ii++) {
-    //
-    //   var sourceDescription = $("<p>", { text: myData.sources[ii].description });
-    //   $(sourceTitle).append(sourceDescription);
-    //
-    // }
-
-
-
-
-    // $(".description").after( $( "h2" ) );
-    // var sourceDescription = $("")
+    textTitle += '<li class=""><a href="' + myData.sources[i].url + '" onclick="myFunction()" target="_blank">' + myData.sources[i].name + '</a>';
+    textTitle += '<p>' + myData.sources[i].description + '</p>';
 
   }
 
-
-
+  textTitle += '<ul>';
+  document.getElementById('sourceContainer').innerHTML = textTitle;
 
   },
   error:function(error){
