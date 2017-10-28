564e971f48885e74ad7f45a35330ccde071923ab
FemkeBuijs <32512274+FemkeBuijs@users.noreply.github.com>
2017-10-28 14:58:55 +0100

    Add favourites on click
    Add the favourite news page when clicking on the add to favourites button. Also added comments to the main.js to make it more readable.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var news1 = require('./routes/news1');
+var favourites = require('./routes/favourites');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/news1', news1);
+app.use('/favourites', favourites);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,20 +1,31 @@
+var posts = {};
+
+//gets the data from the datapage /newsSources created by the router.get in index.js
 $.ajax('/newsSources', {
   method: "GET",
 
   success: function(data){
     var myData = JSON.parse(data);
 
-    var textTitle = '<ul class="">';
+    var textTitle = '<ul class="">'; //creates a variable defined as an unordered list
 
-    for (var i = 0; i < myData.sources.length; i++) {
+    for (var i = 0; i < myData.sources.length; i++) { //loops through the json data retrieved with the AJAX function
 
-      textTitle += '<li class="newslink"><a href="#" data-source="'+ myData.sources[i].id +'">' + myData.sources[i].name + '</a>';
+        //adds a list item to the ul, with a link, paragraph and button. <li> is automatically closed in the loop.
+      textTitle += '<li class="newslink" identifier="' + myData.sources[i].id + '"><a href="#" data-source="'+ myData.sources[i].id +'">' + myData.sources[i].name + '</a>';
       textTitle += '<p>' + myData.sources[i].description + '</p>';
+      textTitle += '<button type = "button" id="favourites"> Add to favourites </button>';
 
+      //adds a property to the global posts variable for the 'current' news page in the loop
+        posts[myData.sources[i].id] = {
+          title: myData.sources[i].name,
+          description: myData.sources[i].description,
+          link: myData.sources[i].url
+        };
     }
 
-    textTitle += '</ul>';
-    $('#sourceContainer').append(textTitle);
+    textTitle += '</ul>'; //adds a closing tag to the textTitle variable
+    $('#sourceContainer').append(textTitle); //appends the data to the div in index.html
 
     },
   error:function(error){
@@ -22,8 +33,28 @@ $.ajax('/newsSources', {
   }
 });
 
+//when clicking the 'Add to favourites' button, the ajax post request sends the specific news source object (within
+// the posts variable) to the favourites.js file, which then saves it to the favourites database.
+
+$('body').on('click', '#favourites', function(){
+    var id = $(this).parent().attr("identifier"); //gets the specific news source property by means of the identifier
+   $.ajax('../favourites', {
+       method: "POST",
+       data: posts[id], //posts the specific news source property by means of the id defined above
+       success: function(data){
+           console.log(data);
+       },
+       error: function(error){
+           console.log('error: ' + error)
+       }
+   })
+});
+
+//When clicking on the news name link, the below ajax request gets the top stories data from the news1 page created
+// by the router.get in news1.js.
+
 $('body').on('click', '.newslink a', function() {
-    $.ajax('/news1/' + $(this).attr('data-source'), {
+    $.ajax('/news1/' + $(this).attr('data-source'), { //gets the top stories from the specific news source
         method: "GET",
 
         success: function(data){
