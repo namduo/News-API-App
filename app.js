db5644e1a9352951709e508716d7299c783c8036
FemkeBuijs <32512274+FemkeBuijs@users.noreply.github.com>
2017-11-03 10:05:57 +0000

    Added newsSources route
    Not currently working

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var news1 = require('./routes/news1');
 var favourites = require('./routes/favourites');
+var newsSources = require('./routes/newsSources');
 
 var app = express();
 
@@ -25,9 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/news1', news1);
 app.use('/favourites', favourites);
+app.use('/newsSources', newsSources);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
