b2c2e7e4a3beb1c8aa1113fa503e935a97c922c0
Nam Nguyen <nm.duong@outlook.com>
2017-11-03 10:15:53 +0000

    updateV3

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var news1 = require('./routes/news1');
 
 var app = express();
@@ -23,9 +22,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', index);
-app.use('/users', users);
 app.use('/news1', news1);
+
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
